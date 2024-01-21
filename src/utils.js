91a556849d1ea41afd992d482eb24d8b5da6be8c
alex-ading <1877693751@qq.com>
2024-01-22 01:11:18 +0800

    feat: replace template params

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,29 @@
 const ora = require('ora');
 const path = require('path');
+const fs = require('fs-extra');
+const handlebars = require('handlebars');
 const clone = require('git-clone/promise');
 
-const downloadTemplate = async (gitAddress, targetDir) => {
-  const distDir = path.join(process.cwd(), targetDir); // 当前输入命令行处的路径 + 目标文件夹
+const updateTemplateParams = (name) => {
+  const filePath = `${name}/package.json`;
+  const fileContent = fs.readFileSync(filePath,'utf-8');
+  const res = handlebars.compile(fileContent)({ name });
+  fs.writeFileSync(filePath, res);
+};
+
+const downloadTemplate = async (gitAddress, projectName) => {
+  const distDir = path.join(process.cwd(), projectName); // 当前输入命令行处的路径 + 目标文件夹
   const loading = ora('正在下载模版...');
-  loading.start();
   try {
+    loading.start();
     await clone(gitAddress, distDir);
-    loading.succeed('创建模版成功!');
-    console.log(`\ncd ${targetDir}`);
+    updateTemplateParams(projectName);
+    loading.succeed('模板创建成功');
+    console.log(`\ncd ${projectName}`);
     console.log('npm i');
     console.log('npm start');
   } catch (e) {
-    loading.fail('创建模版失败：' + e.message);
+    loading.fail('模板创建失败' + e.message);
   }
 };
 
